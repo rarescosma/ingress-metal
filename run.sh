15f2434c2e9daf6533a1f6289a632495a56909e3
Rareș Cosma <rares@getbetter.ro>
2023-01-26 17:47:55 +0100

    format

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 DOT=$(cd -P "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd)
 KC="${1}"
+NS="${NS:-ingress-system}"
+CONFIG_MAP="${CONFIG_MAP:-ingress-metal}"
 
 if ! test -f "$(readlink -f "${KC}")"; then
   echo -e "pass me the path to a kubeconfig"
@@ -19,12 +21,21 @@ trap cleanup TERM INT QUIT EXIT
 
 sudo mkdir -p /var/log/nginx /tmp/nginx
 
-export POD_NAMESPACE=ingress-system
-export POD_NAME=$(kubectl --kubeconfig="${KC}" get pods -n ingress-system -l app.kubernetes.io/sys=ingress-nginx --field-selector=status.phase=Running -o jsonpath='{.items[0].metadata.name}')
+export POD_NAMESPACE="$NS"
+export POD_NAME=$(\
+    kubectl --kubeconfig="${KC}" get pods -n "${NS}" \
+    -l app.kubernetes.io/sys=ingress-nginx \
+    --field-selector=status.phase=Running \
+    -o jsonpath='{.items[0].metadata.name}')
 
 RULES="$(sudo iptables-save)"
-(echo "$RULES" | grep 'ni-block-10254') || sudo iptables -A INPUT -i 'eth0' -p tcp --dport 10254 -m comment --comment 'ni-block-10254' -j DROP 
-(echo "$RULES" | grep 'ni-block-8181') || sudo iptables -A INPUT -i 'eth0' -p tcp --dport 8181 -m comment --comment 'ni-block-8181' -j DROP
+(echo "$RULES" | grep 'ni-block-10254') || sudo iptables -A INPUT -i 'eth0' -p tcp \
+    --dport 10254 -m comment --comment 'ni-block-10254' -j DROP 
+(echo "$RULES" | grep 'ni-block-8181') || sudo iptables -A INPUT -i 'eth0' -p tcp \
+    --dport 8181 -m comment --comment 'ni-block-8181' -j DROP
 
-sudo -E ${DOT}/nginx-ingress-controller --http-port=80 --https-port=443 --kubeconfig "${KC}" --enable-metrics=false --configmap=${POD_NAMESPACE}/ingress-nginx-local
+sudo -E ${DOT}/nginx-ingress-controller --http-port=80 --https-port=443 \
+    --kubeconfig "${KC}" --enable-metrics=false \
+    --election-id=ingress-metal \
+    --configmap=${POD_NAMESPACE}/${CONFIG_MAP}
 
